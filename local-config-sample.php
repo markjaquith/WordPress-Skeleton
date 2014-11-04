ac58b63457b5ccae9fd4b8be66b17e0235456b35
misund <thomas@hemmeligadresse.com>
2014-11-04 22:55:39 +0100

    Moving salts to local config.

--- local-config-sample.php
+++ local-config-sample.php
@@ -4,9 +4,24 @@ This is a sample local-config.php file
 In it, you *must* include the four main database defines
 
 You may include other settings here that you only want enabled on your local development checkouts
-*/
+ */
 
 define( 'DB_NAME', 'local_db_name' );
 define( 'DB_USER', 'local_db_user' );
 define( 'DB_PASSWORD', 'local_db_password' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+define( 'WP_LOCAL_DEV', true );
+
+// ==============================================================
+// Salts, for security
+// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
+// ==============================================================
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
--- wp-config.php
+++ wp-config.php
@@ -11,6 +11,19 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 	define( 'DB_USER', '%%DB_USER%%' );
 	define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
 	define( 'DB_HOST', '%%DB_HOST%%' ); // Probably 'localhost'
+
+	// ==============================================================
+	// Salts, for security
+	// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
+	// ==============================================================
+	define( 'AUTH_KEY',         'put your unique phrase here' );
+	define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+	define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+	define( 'NONCE_KEY',        'put your unique phrase here' );
+	define( 'AUTH_SALT',        'put your unique phrase here' );
+	define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+	define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+	define( 'NONCE_SALT',       'put your unique phrase here' );
 }
 
 // ========================
@@ -24,19 +37,6 @@ define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 // ================================================
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
-
-// ==============================================================
-// Salts, for security
-// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
-// ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
 
 // ==============================================================
 // Table prefix
