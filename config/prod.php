edcef39f044b17f3a4868f5141605f6ed9d010e7
Jie Rong (Felix) <rainmore24@gmail.com>
2015-03-17 00:20:17 +1100

    Changed to use system environment

--- config/prod.php
+++ config/prod.php
@@ -19,8 +19,6 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-**#@-*/
-
 /**
  * WordPress Database Table prefix.
  *
--- wp-config.php
+++ wp-config.php
@@ -1,17 +1,21 @@
 <?php
-// ===================================================
-// Load database info and local development parameters
-// ===================================================
-//
-require_once (sprintf('./config/%s.php', APPLICATION_ENV));
+define('APP_ENV_PROD', 'prod');
+define('APP_ENV_DEV',  'dev');
+
+define('APP_ENV', (getenv('APPLICATION_ENV') === APP_ENV_DEV)
+    ? APP_ENV_DEV : APP_ENV_PROD);
+
+define('APP_WP_CONFIG_PATH', dirname( __FILE__ ));
+
+require_once (sprintf('%s/config/%s.php', APP_WP_CONFIG_PATH, APP_ENV));
 
 // ========================
 // Custom Content Directory
 // ========================
-define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+define( 'WP_CONTENT_DIR', APP_WP_CONFIG_PATH . '/content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
-require_once ('./config/salt.php');
+require_once (APP_WP_CONFIG_PATH . '/config/salt.php');
 
 
 // ================================
@@ -23,8 +27,8 @@ define( 'WPLANG', '' );
 // ======================================
 // Load a Memcached config if we have one
 // ======================================
-if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
-	$memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
+if ( file_exists( APP_WP_CONFIG_PATH . '/memcached.php' ) )
+	$memcached_servers = include( APP_WP_CONFIG_PATH . '/memcached.php' );
 
 // ===========================================================================================
 // This can be used to programatically set the stage when deploying (e.g. production, staging)
@@ -36,5 +40,5 @@ define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack t
 // Bootstrap WordPress
 // ===================
 if ( !defined( 'ABSPATH' ) )
-	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+	define( 'ABSPATH', APP_WP_CONFIG_PATH . '/wp/' );
 require_once( ABSPATH . 'wp-settings.php' );
