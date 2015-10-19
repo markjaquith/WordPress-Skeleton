ffbf573a80138df49d3d3c26859b5580d5d4e7cd
Alexis Mclaren <alexis@raspberrypi.(none)>
2015-10-19 23:56:48 +0100

    Pulled down update to wp

--- local-config-sample.php
+++ local-config-sample.php
@@ -6,7 +6,7 @@ In it, you *must* include the four main database defines
 You may include other settings here that you only want enabled on your local development checkouts
 */
 
-define( 'DB_NAME', 'local_db_name' );
-define( 'DB_USER', 'local_db_user' );
-define( 'DB_PASSWORD', 'local_db_password' );
+define( 'DB_NAME', 'archpress' );
+define( 'DB_USER', 'alexisarches' );
+define( 'DB_PASSWORD', 'totty' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
