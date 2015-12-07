0fc557a3c7c280c4e0659f71cf2c40c481375b43
Emmanuel <sinnell@aol.com>
2015-12-07 16:18:00 +0000

    Added and enqueued css and js files

--- content/themes/elitepress-child/functions.php
+++ content/themes/elitepress-child/functions.php
@@ -1,4 +1,14 @@
 <?php
+/**Theme Name	: elitepress-child
+ * Theme Core Functions and Codes
+*/
+/**Includes reqired resources here**/
+define('WEBRITI_TEMPLATE_DIR_URI', get_stylesheet_directory_uri());
+define('WEBRITI_TEMPLATE_DIR',get_template_directory());
+define('WEBRITI_THEME_FUNCTIONS_PATH',WEBRITI_TEMPLATE_DIR.'/functions');
+
+require_once( WEBRITI_THEME_FUNCTIONS_PATH . '/scripts/scripts.php');
+
 add_action ('wp_enqueue_scripts','theme_enqueue_style');
 function theme_enqueue_style() {
 	wp_enqueue_style ('parent-style', get_template_directory_uri() . '/style.css');
---
+++ content/themes/elitepress-child/js/menu/boot-business.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2012 soundarapandian
+ * Licensed under the Apache License, Version 2.0
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+jQuery("document").ready(function() {
+
+	/*Start: Prevent the default white background on blur of top navigation */
+	jQuery(".dropdown-menu li a").mousedown(function() {
+		var dropdown = $(this).parents('.dropdown');
+		var link = dropdown.children(':first-child');
+		link.css('background-color', "#DB2723");
+		link.css('color', 'white');
+	});
+	/*End: Prevent the default white background on blur of top navigation */
+
+  /*Start : Automatically start the slider */
+	jQuery('.carousel').carousel({
+      interval: 4000
+   });
+	/*End: Automatically start the slider */
+
+});
