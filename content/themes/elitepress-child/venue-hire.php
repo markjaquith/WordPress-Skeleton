9f6588a0790af8e65cc782567721b6e4eb38c12c
Emmanuel <sinnell@aol.com>
2016-01-01 18:18:15 +0000

    added page template for venue hire form

---
+++ content/themes/elitepress-child/venue-hire.php
@@ -0,0 +1,14 @@
+<?php 
+/* Template Name: venue hire */
+get_header();	
+?>
+<div class="content-section">
+	<div class="container">
+		<div class="row
+">
+			
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
