88881f71aadbb8a66d1fd577cf17bee99a084f34
Emmanuel <sinnell@aol.com>
2016-01-03 14:41:55 +0000

    added way to sectioning titles

--- content/themes/elitepress-child/venue-hire.php
+++ content/themes/elitepress-child/venue-hire.php
@@ -1,23 +1,28 @@
-<?php 
+<?php
 /* Template Name: venue hire */
-get_header();	
+get_header();
 ?>
 <div class="content-section">
 	<div class="container">
-		<div class="row">
-			<h1>Venue Enquiry</h1>
-		</div>
-		<div class="row">
-			<div class="col-md-7">
-				<?php get_template_part('content', ''); ?>
+		<div class="title-section">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>Venue Enquiry</h1>
+				</div>
 			</div>
-			<div class="col-md-5">
-				<h3>The Artwork Lounge</h3>
-				<p>Please complete the Venue Enquiry Form to your left if you are interested in using the space at the Arches Project. The Artwork Lounge is a multipurpose hall that has hosted fine art exhibitions, celebrative dinner events, theatre productions and much more. If you are interested please let us know and give details on what you plan (date, time ) and someone will get in touch with you soon.</p>
 			</div>
-			
-		</div>
-	</div>
+			<div class="row">
+				<div class="col-md-6">
+					<?php the_content(); ?>
+				</div>
+				<div class="col-md-5 col-md-offset-1">
+					<h3>The Artwork Lounge</h3>
+					<p>Please complete the Venue Enquiry Form to your left if you are interested in using the space at the Arches Project. The Artwork Lounge is a multipurpose hall that has hosted fine art exhibitions, celebrative dinner events, theatre productions and much more.<br/><br/>If you are interested please let us know and give details on what you plan (date, time ) and someone will get in touch with you soon.</p>
+				</div>
+
+			</div>
+
+  </div>
 </div>
 
 <?php get_footer(); ?>
