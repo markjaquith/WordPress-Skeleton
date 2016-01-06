46c53d44969b4a95adcc9168c5c269ca410a9d9c
Emmanuel <sinnell@aol.com>
2016-01-06 14:41:31 +0000

    applied wordpress hook to remove parent function

--- content/themes/elitepress-child/functions.php
+++ content/themes/elitepress-child/functions.php
@@ -13,6 +13,7 @@ require_once( MY_THEME_FUNCTIONS_PATH . '/scripts/scripts.php');
 add_action ('wp_enqueue_scripts','theme_enqueue_style');
 function theme_enqueue_style() {
 	wp_enqueue_style ('parent-style', get_template_directory_uri() . '/style.css');
+	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
 	wp_enqueue_script('utility', get_stylesheet_directory_uri() .'/js/utility.js');
 }
 
@@ -22,8 +23,13 @@ function my_post_slider_excerpt($output){
 		return '<div class="">' .'<h4>'.$output.'</h4>'.'</div>';
 }
 
-function remove_parent_post_slider_excerpt() {
-    remove_filter('get_the_excerpt','elitepress_post_slider_excerpt');
+function remove_parent_post_slider_excerpt() {	//This function is created to remove and stop the 'parent_post_slider_excerpt' function from executing.   
+    remove_filter('get_the_excerpt','elitepress_post_slider_excerpt'); //This WordPress API hook/function removes the function, elitepress_post_slider_excerpt from the parent.  
 }
-add_action( 'wp_loaded', 'remove_parent_post_slider_excerpt' );
+add_action( 'wp_loaded', 'remove_parent_post_slider_excerpt' ); //This WordPress API hook/function loads and executes the 'remove_parent_post_slider_excerpt' 
+
+function remove_parent_enqueue_scripts() {	//This function is used to remove and stop the 'parent_post_slider_excerpt' function from executing					
+    remove_action('wp_enqueue_scripts','elitepress_scripts'); 
+}
+add_action( 'wp_loaded', 'remove_parent_enqueue_scripts' );
 ?>
