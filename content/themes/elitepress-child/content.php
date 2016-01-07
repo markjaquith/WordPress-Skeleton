14af4b1ba3ed95af65ca2f8f295ab3cfd917486d
Emmanuel <sinnell@aol.com>
2016-01-07 12:22:57 +0000

    removed blog markup

--- content/themes/elitepress-child/content.php
+++ content/themes/elitepress-child/content.php
@@ -1,23 +1,8 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class('blog-area-full'); ?>>
-	<div class="blog-post-img">
-		<?php 
-				// Check Image size for fullwidth template
-				if( is_page_template('blog-full-width.php'))
-				elitepress_image_thumbnail('','img-responsive'); 
-				
-				
-				// Check Image size for Different format like Single post,page
-				elseif(is_single() || is_page())
-				elitepress_post_thumbnail('','img-responsive');
-				
-				else
-				elitepress_post_thumbnail('','img-responsive');	
-		
-				// Close div if page is call
-				if(is_page() )  echo "</div>";
-				
+
+		<?php 				
 				//hide permalink for fullwidth template
-				echo "<div class='blog-info'>"; 
+				echo "<div>"; 
 				if( !is_page_template('fullwidth.php') && get_the_title() && !is_page() ) { ?>
 				<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 				<div class="blog-seprator"></div>
@@ -35,7 +20,7 @@
 				}
 			
                 // call editor content of post/page	
-				echo "<div class='blog-description'>";
+				echo "<div class='page-description'>";
 				the_content();
 				echo "</div>"; 	
 				// allow support for <!--nextpage-->
