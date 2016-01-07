9f208677c794231e5addd585c9b784ec6b53d9df
Emmanuel <sinnell@aol.com>
2016-01-07 12:00:12 +0000

    added content template to display event posts

--- content/themes/elitepress-child/events.php
+++ content/themes/elitepress-child/events.php
@@ -1,3 +1,21 @@
 <?php
-
+/* Template Name: events */
+get_header();
 ?>
+<div class="content-section">
+	<div class="container">
+		<div class="title-section">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>Events</h1>
+				</div>
+			</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8">
+					<?php get_template_part('content',''); ?>
+				</div>
+			</div>
+	</div>
+</div>
+<?php get_footer(); ?>
