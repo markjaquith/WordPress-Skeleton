8bd3da2a45d8cb726d6a6f17bd11b15992f9b102
Emmanuel <sinnell@aol.com>
2016-01-19 16:03:56 +0000

    added performing arts and media template

---
+++ content/themes/elitepress-child/service-media.php
@@ -0,0 +1,6 @@
+<?php /*Template Name: service-media*/ 
+      get_header();	
+?>
+<h1>Filming, Media & Production</h1>
+
+<?php get_footer();?>
--- content/themes/elitepress-child/services.php
+++ content/themes/elitepress-child/services.php
@@ -7,6 +7,23 @@ get_header();
 		<div class="row">
 			<h1>Our Services</h1>
 		</div>
+		<h2>Performing Arts</h2>
+		<p>Improve your skills in one of the many workshops we offer, including: Drama, Music, Art, Vocal Training and more! 
+			If you require any of these services please don't hesitate to contact us </p>
+		<div class="row"><!--Start of row-->
+			<div class="col-md-4 col-sm-12">
+				<div class="thumbnail thumbnail-override">
+					<div class="fix">		
+					  <a href="#"> 
+						<img src="https://placeimg.com/200/170/nature" class="img-responsive" alt="Image" style="width: 100%;">
+					  <div class="desc">	
+						<h3 class="overlay-caption">Performing Arts</h3>
+					  </div>
+					  </a>
+					</div>
+				</div>
+			</div>
+		</div><!--End of row -->
 		<h2>Multimedia Services</h2>
 		<p>The Arches Project offer many services ranging from media production, the arts, music and more!</p>	
 		<div class="row"><!--Start of row-->
@@ -83,47 +100,6 @@ get_header();
 						<h3 class="overlay-caption">Rehearal Space</h3>
 					  </div>
 					 </a>	
-					</div>
-				</div>
-			</div>
-		</div><!--End of row -->
-		<h2>Workshops</h2>
-		<p>Improve your skills in one of the many workshops we offer, including: Drama, Music, Art, Vocal Training and more! 
-			If you require any of these services please don't hesitate to contact us </p>
-		<div class="row"><!--Start of row-->
-			<div class="col-md-4 col-sm-12">
-				<div class="thumbnail thumbnail-override">
-					<div class="fix">		
-					 
-						<img src="https://placeimg.com/200/170/nature" class="img-responsive" alt="Image" style="width: 100%;">
-					  <div class="desc">	
-						<h3 class="overlay-caption">Music</h3>
-					  </div>
-					 
-					</div>
-				</div>
-			</div>
-			<div class="col-md-4 col-sm-12">
-				<div class="thumbnail thumbnail-override">
-					<div class="fix">		
-					 	
-						<img src="https://placeimg.com/200/170/nature" class="img-responsive" alt="Image" style="width: 100%;">
-					  <div class="desc">	
-						<h3 class="overlay-caption">Art</h3>
-					  </div>
-					 	
-					</div>
-				</div>
-			</div>
-			<div class="col-md-4 col-sm-12">
-				<div class="thumbnail thumbnail-override">
-					<div class="fix">		
-					 	
-						<img src="https://placeimg.com/200/170/nature" class="img-responsive" alt="Image" style="width: 100%;">
-					  <div class="desc">	
-						<h3 class="overlay-caption">Photography</h3>
-					  </div>
-						
 					</div>
 				</div>
 			</div>
