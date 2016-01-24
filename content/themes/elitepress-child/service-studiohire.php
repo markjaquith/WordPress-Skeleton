fd750d747fd4c30c6c0551e8c9bbb40e3f5f848a
Emmanuel <sinnell@aol.com>
2016-01-24 15:46:43 +0000

    added studio hire service page

---
+++ content/themes/elitepress-child/service-studiohire.php
@@ -0,0 +1,32 @@
+<?php 
+/* Template Name: service-studio */
+ get_header();
+?>
+<div class="content-section">
+	<div class="container">
+	 <div class="title-section"> 
+	  <div class="row">
+	   <div class="col-md-12">
+		<h1>Studio Hire</h1>
+	   </div>
+	  </div>
+	 </div>
+	 <div class="row">
+		<p>Visual media plays an important part in promotion of companies - with online services like YouTube, Vimeo and Facebook, Twitter already having established communities, it's essential to have your presence made to increase your audience and engage them to your service. 
+Our Media department can create a digitally visual image for your company through a variety of mediums. Whether you need a short video promoting your identity or campaign, promoting your enterprise on a variety of social medias and blogs</p>
+		<ul>
+			<li>Catchy Promotional Adverts</li>
+			<li>Short Films</li>
+			<li>Event filming (Music Performances, Theatre Productions, Performing Arts and more)</li>
+			<li>Full 1080p High Definition</li>
+			<li>Post-Production</li>
+			<li>Film Shoot</li>
+			<li>Post-Production editing</li>
+			<li>Upload to YouTube, Vimeo and other networks</li>
+		</ul>
+		<p>If you require any media services, please e-mail us at services@thearchesproject.org and we will respond to you as soon as we can.</p>
+	</div>
+	</div>
+</div>	
+
+<? get_footer(); ?>
