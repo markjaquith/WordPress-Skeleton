9a0c373a2b03a8902c86239af22290e7f93f78a7
Emmanuel <sinnell@aol.com>
2016-01-24 22:32:07 +0000

    constrained text to col-md-10 width

--- content/themes/elitepress-child/services.php
+++ content/themes/elitepress-child/services.php
@@ -10,10 +10,14 @@ get_header();
 			<h1>Our Services</h1>
 		  </div>	
 		</div>
-	   </div>	
+	   </div>
+	   <div class="row">
+		<div class="col-md-10">
 		<h2>Arts</h2>
 		<p>Improve your skills in one of the many workshops we offer, including: Drama, Music, Art, Vocal Training and more! 
 			If you require any of these services please don't hesitate to contact us </p>
+		</div>	
+	   </div>	
 		<div class="row"><!--Start of row-->
 			<div class="col-md-4 col-sm-12">
 				<div class="thumbnail thumbnail-override">
@@ -28,8 +32,12 @@ get_header();
 				</div>
 			</div>
 		</div><!--End of row -->
+	   <div class="row">
+		<div class="col-md-10">
 		<h2>Multimedia Services</h2>
-		<p>The Arches Project offer many services ranging from media production, the arts, music and more!</p>	
+		<p>The Arches Project offer many services ranging from media production, the arts, music and more!</p>
+        </div>
+       </div> 		
 		<div class="row"><!--Start of row-->
 			<div class="col-md-4 col-sm-12">
 				<div class="thumbnail thumbnail-override">
@@ -68,8 +76,12 @@ get_header();
 				</div>
 			</div>
 		</div><!--End of row -->
+	  <div class="row">
+		<div class="col-md-10">	
 		<h2>Space</h2>
 		<p>Take advantage of our many spaces and studios - perfect for events, photo shoots, recording and rehearsing, we provide professional surroundings and ideal sizes! </p>
+		</div>
+	  </div>
 		<div class="row"><!--Start of row-->
 			<div class="col-md-4 col-sm-12">
 				<div class="thumbnail thumbnail-override">
