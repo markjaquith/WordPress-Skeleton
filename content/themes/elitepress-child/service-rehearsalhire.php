e7eda0190ab062408e65ac9c0b05633ac5527277
Emmanuel <sinnell@aol.com>
2016-01-24 15:54:35 +0000

    added rehearsal hire page

---
+++ content/themes/elitepress-child/service-rehearsalhire.php
@@ -0,0 +1,31 @@
+<?php 
+/* Template Name: service-rehearsal */
+ get_header();
+?>
+<div class="content-section">
+	<div class="container">
+	 <div class="title-section"> 
+	  <div class="row">
+	   <div class="col-md-12">
+		<h1>Rehearsal Hire</h1>
+	   </div>
+	  </div>
+	 </div>
+	 <div class="row">
+		<p>Studios are available for recording music, monologues, narration or any auditory projects. It's important that sound recording be as crisp and audible as possible, so our space allows you to work in a professional atmosphere, progressing your abilities further and ensuring your sound is at the highest quality it deserves.</p>
+		<ul>
+			<li>Catchy Promotional Adverts</li>
+			<li>Short Films</li>
+			<li>Event filming (Music Performances, Theatre Productions, Performing Arts and more)</li>
+			<li>Full 1080p High Definition</li>
+			<li>Post-Production</li>
+			<li>Film Shoot</li>
+			<li>Post-Production editing</li>
+			<li>Upload to YouTube, Vimeo and other networks</li>
+		</ul>
+		<p>If you require any media services, please e-mail us at services@thearchesproject.org and we will respond to you as soon as we can.</p>
+	</div>
+	</div>
+</div>	
+
+<? get_footer(); ?>
