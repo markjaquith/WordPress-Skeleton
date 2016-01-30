8982f02b9f1407d61c24f9146a248b83c4731d87
Emmanuel <sinnell@aol.com>
2016-01-30 02:43:51 +0000

    styled and added buddpress and mediapress default template

--- content/themes/elitepress-child/buddypress.php
+++ content/themes/elitepress-child/buddypress.php
@@ -2,6 +2,14 @@
 /* Template Name: buddypress */
 get_header();
  ?>
-
+<div class="content-section">
+	<div class="title-section">
+	</div>
+	<div class="container">
+		<div class="col-md-10">
+			<?php get_template_part('content',''); ?>
+		</div>
+	</div>
+</div>	
 
 <?php get_footer(); ?>
