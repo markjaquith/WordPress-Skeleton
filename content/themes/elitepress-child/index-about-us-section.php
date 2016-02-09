f79c229e7a43dc1f643645b3093f74ff25925f71
Emmanuel <sinnell@aol.com>
2016-02-09 18:30:55 +0000

    added about us page template

--- content/themes/elitepress-child/index-about-us-section.php
+++ content/themes/elitepress-child/index-about-us-section.php
@@ -12,25 +12,17 @@
 					</div>
 					<div class = "row">
 						<div class = "col-md-6 col-sm-12">
-							<div>
-								<p>The Arches Project has been founded with the sight to be a platform for creative talents of all discipline.</p>
+							<div id="about-text">
+								<p>The Arches Project has been founded with the sight <a href="#">to be a platform</a> for creative talents of all discipline.</p>
 								<p>
 									It gives emerging artists the opportunity to gain experience within the industry and achieve exposure they would not otherwise have received.
 We believe too many creative talents give up on their passion for what they have worked so hard to achieved as a result of the lack of ability to expose their work, with your donations we will be able to give more support to these artists whilst offering a better chance in life, providing opportunities to enable them to develop leadership skills, build self confidence, encourage and develop positive attitudes to pursue their aspirations.
 								</p>
 								<p>This passion is too important to lose faith in.</p>
 							</div>
-							<div>
-								<p>
-									We have reflected all sorts of talent, including the performing arts, showcasing Romeo and Juliet, and hamlet by Shakespeare, open mic sessions, comedy shows and music events.
-We have had interactive and visual design exhibition by students attending Birmingham Metropolitan College, fine art exhibition by students of Margaret Street School of art, Birmingham, Photography exhibition by South and City College also private exhibitions where the work of established and emerging artists is shown in this exciting and unique space in the heart of Birmingham.
-
-								</p>
-								<p>This exposure from our services has led artists in finding full time employment within the industry they are passionate about.</p>
-							</div>
 						</div>
 						<div class ="col-md-4 col-sm-12 col-md-offset-1">
-							<?php echo do_shortcode('[custom-facebook-feed id=TheArchesProject num=2 type=photos include=likebox,text,date,eventtitle,author]'); ?>
+							<?php echo do_shortcode('[custom-facebook-feed id=TheArchesProject num=1 type=photos include=likebox,text,date,eventtitle,author]'); ?>
 						</div>
 					</div>		
 					</div>
