b0e0a1211bfa0495a81ca2f414f1d9fbedbdcdf5
Emmanuel <sinnell@aol.com>
2016-02-10 12:52:05 +0000

    added apply template and content

---
+++ content/themes/elitepress-child/apply.php
@@ -0,0 +1,48 @@
+<?php
+/* Template Name: how to apply*/
+get_header();
+?>
+<div class="content-section">
+	<div class="container">
+		<div class="title-section">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>Becoming an Artist Member</h1>
+				</div>
+			</div>
+		</div>
+    <div class="row">
+      <div class="col-md-12">
+        <p>The Arches Project continues to expand our themes of creativity, allowing everyone with a talent that deserves to be seen to get this exposure. We host YouTube gatherings, photography and fine art exhibitions, we have a variety of up coming exhibitions such as Hidden Talents which is specifically aimed at uncovering talents that are yet to be seen and also Extravagant Imaginations, based on fantasy.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <h3>Call out for Artists</h3>
+        <p>We are seeking proposals from visual artists working in a variety of disciplines who would like to exhibit their work in an open exhibition. <p>
+        <ul>
+          <li>You must be prepared for your work to be displayed in a business setting</li>
+          <li>Work may be for sale if required (Advice can 	be given on pricing)</li>
+          <li>Artists may be requested to assist on rotation in staffing during the opening hours of exhibition of event</li>
+          <li>All artist must be in attendance on the opening/preview evening</li>
+          <li>To exhibit in any of our exhibitions you will need to register on our artist directory</li>
+        </ul>
+        <p>Attaining an online Artist Membership is on a <strong>invitation only basis</strong>, so if you are a painter, illustrator, photographer, singer, comedian, creative writer or a sculpture? Or have any other talent that you think needs to be seen, then <strong><a href="<?php echo site_url('contact-us') ?>">contact us</a></strong> online today or email us at: <strong><a href="mailto:submissions@thearchesproject.org?Subject=submission%20enquiry">submissions@thearchesproject.org</a></strong> </p>
+      </div>
+      <div class="col-md-6">
+        <h3>Fees</h3>
+        <p>To sign up and register on the artist directory is free until 31st July 2016 after this date registration fee will be £30 for 12 months.<br>Exhibiting artwork fees starts from £10 per piece of work depending on size of work.</p>
+        <p>All artwork sold a commission of 30% will be due from sales and the remainder paid to artists within 2 weeks by Bank Transfer</p>
+        <p>We will promote our members on our social media pages and our website with the opportunity of our members having access to upload their work on our site, invitations to all exhibitions in-house as well at other spaces, competitions  and functions held by the Arches Project giving you the opportunity for your work to be seen  and get recognised.</p>
+        <p>Your work is also promoted to specific clients in the industry and featured on our promotional materials.</p>
+        <ul>
+          <li>Smoking is not permitted in foyers/gallery</li>
+          <li>No filming/photographs are permitted without artists consent</li>
+          <li>No recording or filming within the exhibition space</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
