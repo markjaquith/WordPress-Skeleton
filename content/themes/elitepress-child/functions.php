41a733fd179646e5aaa104aff0926411a33786b7
Emmanuel <sinnell@aol.com>
2016-02-10 13:16:49 +0000

    added group member upload restriction feature

--- content/themes/elitepress-child/functions.php
+++ content/themes/elitepress-child/functions.php
@@ -17,6 +17,7 @@ function theme_enqueue_style() {
 	wp_enqueue_script('utility', get_stylesheet_directory_uri() .'/js/utility.js');
 }
 
+//Filters
 //Functions dealing with manipulating the excerpt of posts
 add_filter('get_the_excerpt','my_post_slider_excerpt');
 function my_post_slider_excerpt($output){
@@ -34,17 +35,33 @@ function my_excerpt_more( $more ) {
 }
 add_filter( 'excerpt_more', 'my_excerpt_more', 1000 );
 
-//Functions and methods for removing and stopping wordpress from loading parent functions 
-function remove_parent_post_slider_excerpt() {	//This function is created to remove and stop the 'parent_post_slider_excerpt' function from executing.   
-    remove_filter('get_the_excerpt','elitepress_post_slider_excerpt'); //This WordPress API hook/function removes the function, elitepress_post_slider_excerpt from the parent.  
+//Functions and methods for removing and stopping wordpress from loading parent functions
+function remove_parent_post_slider_excerpt() {	//This function is created to remove and stop the 'parent_post_slider_excerpt' function from executing.
+    remove_filter('get_the_excerpt','elitepress_post_slider_excerpt'); //This WordPress API hook/function removes the function, elitepress_post_slider_excerpt from the parent.
 }
-add_action( 'wp_loaded', 'remove_parent_post_slider_excerpt' ); //This WordPress API hook/function loads and executes the 'remove_parent_post_slider_excerpt' 
+add_action( 'wp_loaded', 'remove_parent_post_slider_excerpt' ); //This WordPress API hook/function loads and executes the 'remove_parent_post_slider_excerpt'
 
-function remove_parent_enqueue_scripts() {	//This function is used to remove and stop the 'parent_post_slider_excerpt' function from executing					
-    remove_action('wp_enqueue_scripts','elitepress_scripts'); 
+function remove_parent_enqueue_scripts() {	//This function is used to remove and stop the 'parent_post_slider_excerpt' function from executing
+    remove_action('wp_enqueue_scripts','elitepress_scripts');
 }
 add_action( 'wp_loaded', 'remove_parent_enqueue_scripts' );
 
+function mpp_custom_restrict_group_upload( $can_do, $component, $component_id, $gallery  ) {
+
+	if ( $component != 'groups' ) {
+		return $can_do;
+	}
+	//we only care about group upload
+	$gallery = mpp_get_gallery( $gallery );
+
+	if ( ! $gallery || $gallery->user_id != get_current_user_id() ) {
+		return false;//do not allow if gallery is not given
+	}
+
+	return true;//the user had created this gallery
+
+}
+add_filter( 'mpp_user_can_upload', 'mpp_custom_restrict_group_upload', 11, 4 );
 //shortcodes
  //start of shortcode
 function bpProfile( $atts=null, $content=null ) { //shortcode for returning the url of an artist members profile
