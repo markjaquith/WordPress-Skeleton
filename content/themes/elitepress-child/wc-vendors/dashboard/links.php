dd93ab6c7100bdb2ea24ee80cf09383396f8ce93
Emmanuel <sinnell@aol.com>
2016-02-17 15:01:39 +0000

    added alert when shop name is not set

--- content/themes/elitepress-child/wc-vendors/dashboard/links.php
+++ content/themes/elitepress-child/wc-vendors/dashboard/links.php
@@ -10,3 +10,12 @@
 </center>
 
 <hr>
+<?php
+$author_id = get_current_user_id();
+$shop_name_set = WCV_Vendors::get_vendor_shop_name( $author_id );
+$vendor_login = get_userdata($author_id);
+if ($shop_name_set == $vendor_login->user_login) { //outputs warning to vendor if they haven't configured their shop settings
+  echo '<div class="alert alert-warning"><h4>You haven\'t configured your store name!</h4><p>Also make sure to set your paypal email address in the shop settings to receive payments.<br><br><a href="'.$settings_page.'">Click here to configure your store</a>.</p></div>';
+}
+
+?>
