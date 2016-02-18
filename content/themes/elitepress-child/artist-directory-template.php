aeb2d0da11ee09b9c87543ccc74d45eaef48470e
Emmanuel <sinnell@aol.com>
2016-02-18 20:44:21 +0000

    added fix for button styles

--- content/themes/elitepress-child/artist-directory-template.php
+++ content/themes/elitepress-child/artist-directory-template.php
@@ -1,17 +1,17 @@
 <?php
 /* Template Name: artist directory template */
 get_header();
-?>	
+?>
 	<div class="content-section">
 		<div class="container">
 			<div class="title-section">
 				<div class="row">
 					<div class="col-md-6">
 						<h1>Artist Directory</h1>
-					</div>	
+					</div>
 				</div>
 			</div>
-		
+
 			<div class="row">
 				<div class="col-md-6">
 					<p>
@@ -20,30 +20,30 @@ get_header();
 					<p>
 						We are also actively seeking musicians and performers for up and coming events and workshops to help them gain experience and excel further within their chosen field at our rehearsal space in Digbeth. We aim to hold many more events for our up and coming musicians and performers so they can excel further in their talent, we give them the ability to do this by providing a space for them to practice and develop their skills in their own time.
 					</p>
-					<button type="button" class="btn btn-apply" style="margin-top: 15px;" data-toggle="button" aria-pressed="false" autocomplete="off">
+					<button type="button" class="my-btn btn-apply" style="margin-top: 15px;" data-toggle="button" aria-pressed="false" autocomplete="off">
 					Apply
 					</button>
-					<button type="button" class="btn btn-apply" style="margin-left: 15px; margin-top: 15px;" data-toggle="button" aria-pressed="false" autocomplete="off">
-					View Artist Shop 
+					<button type="button" class="my-btn btn-apply" style="margin-left: 15px; margin-top: 15px;" data-toggle="button" aria-pressed="false" autocomplete="off">
+					View Artist Shop
 					</button>
-					
+
 				</div>
 				<div class="col-md-6">
-				
+
 					<div class="row">
 						<div class="col-md-6">
 							<a href="photography">
 								<img src="http://placehold.it/100x30?text=Photography" class="img-responsive" alt="Image" style="width: 100%;"/>
 							</a>
-						</div>	
+						</div>
 						<div class="col-md-6">
 							<a href="sculpture">
 								<img src="http://placehold.it/100x30?text=Sculpture" class="img-responsive" alt="Image" style="width: 100%;"/>
 							</a>
-						</div>	
-					</div>		
+						</div>
+					</div>
 					<hr>
-				
+
 					<div class="row">
 						<div class="col-md-6">
 							<a href="music">
@@ -57,35 +57,35 @@ get_header();
 						</div>
 					</div>
 					<hr>
-					
+
 					<div class="row">
 						<div class="col-md-6">
 							<a href="vocal-music">
 								<img src="http://placehold.it/100x30?text=Vocal+music" class="img-responsive" alt="Image" style="width: 100%;"/>
 							</a>
 						</div>
-						<div class="col-md-6">	
+						<div class="col-md-6">
 							<a href="mixed-media-and-collage">
 								<img src="http://placehold.it/100x30?text=Mixed+media+and+collage" class="img-responsive" alt="Image" style="width: 100%;"/>
 							</a>
-						</div>	
+						</div>
 					</div>
 					<hr>
-					
+
 					<div class="row">
 						<div class="col-md-6">
 							<a href="installation">
 								<img src="http://placehold.it/100x30?text=Installation" class="img-responsive" alt="Image" style="width: 100%;"/>
 							</a>
-						</div>	
+						</div>
 						<div class="col-md-6">
 							<a href="graphic-design">
 								<img src="http://placehold.it/100x30?text=Graphic+design" class="img-responsive" alt="Image" style="width: 100%;"/>
 							</a>
 						</div>
-					</div>		
+					</div>
 					<hr>
-					
+
 					<div class="row">
 						<div class="col-md-6">
 							<a href="artist-directory/illustration">
@@ -96,21 +96,20 @@ get_header();
 							<a href="artist-directory/drawing-and-painting">
 								<img src="http://placehold.it/100x30?text=Drawing+and+painting" class="img-responsive" alt="Image" style="width: 100%;"/>
 							</a>
-						</div>	
+						</div>
 					</div>
 					<hr>
-					
+
 					<div class="row">
 						<div class="col-md-6">
 							<a href="artist-directory/short-film">
 								<img src="http://placehold.it/100x30?text=Short+film" class="img-responsive" alt="Image" style="width: 100%;"/>
 							</a>
-						</div>	
-					</div>		
+						</div>
+					</div>
 				</div>
-				
+
 			</div>
 		</div>
 	</div>
-<?php get_footer(); ?> 		
-
+<?php get_footer(); ?>
