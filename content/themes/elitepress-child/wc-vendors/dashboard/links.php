e437dd787ebdff86681db7b0190e5d41bd7e4bba
Emmanuel <sinnell@aol.com>
2016-02-20 14:01:12 +0000

    vendor dashboard links now link to front-end only

--- content/themes/elitepress-child/wc-vendors/dashboard/links.php
+++ content/themes/elitepress-child/wc-vendors/dashboard/links.php
@@ -2,10 +2,13 @@
 <p class="vendor-links">
         <a href="<?php echo $shop_page; ?>" class="button"><?php echo _e( 'View Your Store', 'wcvendors' ); ?></a>
         <a href="<?php echo $settings_page; ?>" class="button"><?php echo _e( 'Store Settings', 'wcvendors' ); ?></a>
-
+<?php
+$wcv_profile_id = get_current_user_id(); //gets current user id
+$profile_url = bp_core_get_user_domain ( $wcv_profile_id ); //gets url of members profile dashboard based on current user id
+?>
 <?php if ( $can_submit ) { ?>
-                <a target="_TOP" href="<?php echo $submit_link; ?>" class="button"><?php echo _e( 'Add New Product', 'wcvendors' ); ?></a>
-                <a target="_TOP" href="<?php echo $edit_link; ?>" class="button"><?php echo _e( 'Edit Products', 'wcvendors' ); ?></a>
+                <a target="_TOP" href="<?php echo $profile_url.'products/products-create'; ?>" class="button"><?php echo _e( 'Add New Product', 'wcvendors' ); ?></a>
+                <a target="_TOP" href="<?php echo $profile_url.'products/products-my-posts'; ?>" class="button"><?php echo _e( 'Edit Products', 'wcvendors' ); ?></a>
 <?php } ?>
 </center>
 
