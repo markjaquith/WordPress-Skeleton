a80bc41364cbc2ea4e90e52f3819f96f742d81ad
Emmanuel <sinnell@aol.com>
2016-02-23 15:24:29 +0000

    change from number of trial days to weeks

--- content/themes/elitepress-child/artist-drawingpainting.php
+++ content/themes/elitepress-child/artist-drawingpainting.php
@@ -7,7 +7,7 @@ get_header();
 		<div class="title-section">
 			<div class="row">
 				<div class="col-md-12">
-					<h2>Drawing and painting</h2>
+					<h2>Drawing and Painting</h2>
 				</div>
 			</div>
 		</div>
@@ -15,7 +15,7 @@ get_header();
 			<div class="col-md-12">
 				<?php echo do_shortcode('[mpp-list-gallery component=groups component_id=2 view="grid"]'); ?>
 			</div>
-		</div>		
+		</div>
 	</div>
 </div>
 <?php get_footer(); ?>
